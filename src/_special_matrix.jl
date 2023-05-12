f2653182fc71ff13180b18ecc8927236802fb575
Hiroyuki Okamura <okamu@hiroshima-u.ac.jp>
2023-05-12 18:42:15 +0900

    modify eye

--- src/_special_matrix.jl
+++ src/_special_matrix.jl
@@ -4,14 +4,18 @@ Special matrix
 
 """
 eye(n, ::Type{Tv} = Float64)::Matrix{Tv}
+eye(A::AbstractMatrix, ::Type{Tv} = Float64)::Matrix{Tv}
 
 Make an indentity matrix
 """
-
 function eye(n, ::Type{Tv} = Float64)::Matrix{Tv} where {Tv}
     m = zeros(Tv, n,n)
     @inbounds for i = 1:n
         m[i,i] = Tv(1)
     end
     m
+end
+
+function eye(A::AbstractMatrix, ::Type{Tv} = Float64)::Matrix{Tv} where {Tv}
+    eye(size(A)[1])
 end
